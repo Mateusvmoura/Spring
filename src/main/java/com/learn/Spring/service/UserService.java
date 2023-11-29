684876dd846f9681f2244e88df1463913543c47c
Mateus Moura <108039511+Mateusvmoura@users.noreply.github.com>
2023-11-29 14:56:39 -0300

    Create UserService

---
+++ src/main/java/com/learn/Spring/service/UserService.java
@@ -0,0 +1,50 @@
+package com.learn.Spring.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.learn.Spring.models.User;
+import com.learn.Spring.repositories.TaskRepository;
+import com.learn.Spring.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository; 
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("Usuario não encontrado! "));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delet(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas");
+        }
+    }
+}
